ff3204e772d8237fa6ea6f07f2fb262e586e023b
Lornis Hervilla <lhervi@gmail.com>
2023-03-07 11:07:58 -0400

    primer commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts/base')
+
+@section('name', 'Lornis')
+
+@section('content')
+    <h1 class="text-4xl font-bold">About</h1>
+    
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+
+@extends('layouts/base')
+
+@section('name', 'Lornis')
+
+@section('content')
+    <h1 class="text-4xl font-bold">Home</h1>
+    <p class="mt-4 text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Nullam vel ultrices eros, nec viverra nulla. Duis felis est, blandit nec consequat non, 
+        luctus eu enim. Sed ut risus nisi. Cras quis augue tincidunt, sagittis elit eu, tincidunt risus. 
+        In ac massa quam. Praesent tristique lorem at dolor ultricies facilisis
+    </p>
+@endsection
